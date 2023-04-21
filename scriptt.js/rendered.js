9ac74bbc9ba5fe54ac390886da0ce8760fadd061
Sameer Chauhan <sameerchauhan212204@gmail.com>
2023-04-21 18:53:34 +0530

    rendering the ui with data

--- scriptt.js/rendered.js
+++ scriptt.js/rendered.js
@@ -1,23 +1,34 @@
-const renderui=(suares)=>{
+const renderui=(squares)=>{
     // maincontainer from markup
     const maincontainer = document.querySelector('.container')
-    console.log(maincontainer)
+    // console.log(maincontainer)
 
-    console.log(squares);
-
-    squares.forEach((element) => {
-        let rank=document.createElement("div");
-        rank.classList.add('rank-style');
-        maincontainer.appendChild(rank);
-        element.forEach(el=>{
-            const square =document.createElement('div');
-            square.classList.add(`color-${el.color}`);
-            square.classList.add()
-            el.renderedhtml=square;
-             console.log(el)
-        })
+    // console.log(squares);
+    let rankno=8;
+   // create square of size 70 x 70    
+   squares.forEach(element => {
+    let rank = document.createElement("div");
+    rank.classList.add('rank-style');
+    rank.setAttribute('id','i'+rankno--)
+    maincontainer.appendChild(rank);
+    element.forEach(el =>{
+        const square = document.createElement('div');
+        square.style.backgroundColor = square.color;
+        el.renderedHtml  = square;
+        console.log(el)
+        square.classList.add(`color-${el.color}`);
+        square.classList.add('square-common');
         
+        // 
+        square.setAttribute('id',el.id)
+
+        square.classList.add('file-'+el.id[0])
+        rank.appendChild(square);
     });
 
+});
+console.log(maincontainer);
+
 }
-export{renderui}
+
+export {renderui};        
--- scriptt.js/script.js
+++ scriptt.js/script.js
@@ -9,8 +9,14 @@ class Square {
 }
 
 for (let i = 8; i > 0; i--) {
+
+   const isroweven =i%2==0? true:false
+
     const rowArray = [];
     for (let j = 97; j < 105; j++) {
+
+      const iseleven=j%2==0? true:false
+
         const fileName = String.fromCharCode(j);
         const id = fileName + i;
         // create object of squares
@@ -22,9 +28,21 @@ for (let i = 8; i > 0; i--) {
         // color
         // odd row => odd element
         // (769656)
+        if(!isroweven && !iseleven){
+          square.color='dark';
+        }
         // even row => odd element (EEEED2)
+        if(isroweven && !iseleven){
+          square.color='light';
+        }
         // odd row => even element
+        if(!isroweven && iseleven){
+          square.color='light';
+        }
         // even row => odd element (EEEED2)
+        if(isroweven && iseleven){
+          square.color='dark';
+        }
         // even row => even element
         // (769656)
 
@@ -37,4 +55,6 @@ for (let i = 8; i > 0; i--) {
 }
 
 console.log(chararr);
-  
+
+import * as ness from "../scriptt.js/rendered.js";
+ness.renderui(chararr)
