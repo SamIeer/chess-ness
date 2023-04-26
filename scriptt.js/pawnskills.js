d11bdece90c48400997d7dd2db30b044f60e4646
Sameer Chauhan <sameerchauhan212204@gmail.com>
2023-04-26 19:21:25 +0530

    solved set

--- scriptt.js/pawnskills.js
+++ scriptt.js/pawnskills.js
@@ -1,22 +1,22 @@
 
 
-export let Pmoves = (el, psing) => {
+export let Pmoves = (el, psing,square) => {
     const row = Number(el.id[1])
     const column = el.id[0]
   
     //  for white castle pawns
     if (row === 2) {
-      front2(row, column, psing)
+      front2(row, column, psing,square)
     }
   
     // for black castle pawns
     else if (row === 7) {
-      front2(row, column, psing)
+      front2(row, column, psing,square)
     }
   }
   
   // function for the initial movements of pawn
-  const front2 = (row, column, psing) => {
+  const front2 = (row, column, psing,square) => {
     let pawnar = []
     let newrow = row
     if (newrow === 2) {
@@ -43,24 +43,28 @@ export let Pmoves = (el, psing) => {
   
     for (const i of pawnar) {
       const myTag = document.getElementById(i);
-    //   console.log(myTag)
       const dot = document.createElement("div");
       myTag.appendChild(dot);
       dot.classList.add("dot");
-      const c=pawnar.indexOf[i]
-      console.log(c)
-      // moving the pawn and removing the dot
       
-      if(pawnar.indexOf(i)==1)
-      {
-        myTag.addEventListener('click', () => {
-            myTag.innerHTML = psing.innerHTML;
-            document.getElementById(removecircle[0]).innerHTML = '';
-            const removecircle = pawnar.filter((el) => el != i);
-            psing.textContent = '';
-            console.log(pawnar.indexOf(i))
-          }, {
-            once: true
-          })
-        }}
+      if (pawnar.indexOf(i) === 1 || pawnar.indexOf(i) === 0) {
+        myTag.addEventListener('click', handleClick);
+      }
+    }
+    
+    function handleClick() {
+      const clickedElement = this;
+      
+      // Remove event listener from both elements
+      for (const i of pawnar) {
+        const element = document.getElementById(i);
+        element.removeEventListener('click', handleClick);
+      }
+      
+      clickedElement.innerHTML = psing.innerHTML;
+      const removecircle = pawnar.filter((el) => el !== clickedElement.id);
+      document.getElementById(removecircle[0]).innerHTML = '';
+      square.removeChild(psing)
+    }
+    
   }
